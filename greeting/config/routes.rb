53c5b1b4593ba64b6ddb89ce3e4041fc6367b553
Brenston Treaudo <btreaudo@gmail.com>
2015-08-11 08:49:54 -0500

    Adding stuff

--- greeting/config/routes.rb
+++ greeting/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'Say#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
